a4a622a8c1d786d6dc72f34e4f34ff63fc1f02a8
Naol Chala <naolchala6@gmail.com>
2025-05-27 18:24:02 +0300

    Initialize monorepo app with shadcn

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@workspace/ui/components/button";
+import { Badge } from "@workspace/ui/components/badge";
+
+export default function Page() {
+	return (
+		<div className="flex items-center justify-center min-h-svh">
+			<div className="flex flex-col items-center justify-center gap-4">
+				<h1 className="text-2xl font-bold">Hello World</h1>
+				<Button size="sm">Button</Button>
+				<Badge>Naol</Badge>
+			</div>
+		</div>
+	);
+}
